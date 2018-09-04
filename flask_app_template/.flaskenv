b58da201e66f4e56821e5a912bc3375b0f181c09
angel <xxStarcat@gmail.com>
2018-09-03 22:11:30 -0400

    add

--- flask_app_template/.flaskenv
+++ flask_app_template/.flaskenv
@@ -1,6 +1,16 @@
 FLASK_APP=example.py
 
 FLASK_ENV=development
+FLASK_DEBUG=0
 
 SECRET_KEY=not-that-secret
 DATABASE_URL=sqlite:////absolute/path/to/db
+
+MAIL_SERVER=smtp.googlemail.com
+MAIL_PORT=587
+MAIL_USE_TLS=1
+MAIL_USERNAME=
+MAIL_PASSWORD=
+
+DEV_MAIL_SERVER=localhost
+DEV_MAIL_PORT=8025
--- flask_app_template/config.py
+++ flask_app_template/config.py
@@ -7,3 +7,11 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') if os.environ.get(
+        'FLASK_ENV') == 'production' else os.environ.get('DEV_MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') if os.environ.get('FLASK_ENV')
+                    == 'production' else os.environ.get('DEV_MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['your-email@example.com']
